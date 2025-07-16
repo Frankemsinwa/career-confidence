d93a57e35dfbc3f121981701fc5724f6233a4b7a
Frank Emesinwa <frankemesinwa@gmail.com>
2025-07-16 12:50:22 +0000

    so the user will be signin and signup with also google provider so add t

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,3 +1,4 @@
+
 'use server';
 
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
@@ -16,21 +17,19 @@ export function createSupabaseServerClient(): SupabaseClient {
     );
   }
 
-  const cookieStore = cookies();
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value;
+          return cookies().get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options });
+          cookies().set({ name, value, ...options });
         },
         remove(name: string, options: CookieOptions) {
-          cookieStore.set({ name, value: '', ...options });
+          cookies().set({ name, value: '', ...options });
         },
       },
     }
