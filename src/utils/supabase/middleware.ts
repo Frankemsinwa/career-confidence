3fb5b55bfe4a6ee1caee7f1a6824193ff828fd21
Frank Emesinwa <frankemesinwa@gmail.com>
2025-07-16 13:36:47 +0000

    configure it to fit our project

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -40,9 +40,9 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     request.nextUrl.pathname !== '/' &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/api/auth')
   ) {
-    // no user, potentially respond by redirecting the user to the login page
+    // no user, redirect to the login page
     const url = request.nextUrl.clone()
     url.pathname = '/'
     return NextResponse.redirect(url)
